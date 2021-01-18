eae2587cb23e569d7e4f3c9ac01ad2361d52217b
Jackson Hansel <hanseljt@hanseljt-5520.rose-hulman.edu>
2021-01-18 13:43:00 -0500

    view is there with hard coded data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserlistComponent } from './userlist/userlist.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CaseComponent } from './case/case.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'userlist', component: UserlistComponent },
+  { path: 'case', component: CaseComponent}
 
 ];
 
